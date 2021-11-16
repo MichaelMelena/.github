c5f2eee8dddd9f0e1cccd95050b63122abbd30fc
Michael Melena <melena@havit.cz>
2021-11-16 18:29:01 +0100

    fix bee http

--- data/get_data.js
+++ data/get_data.js
@@ -2,6 +2,9 @@ const request = require('request');
 const fs = require('fs');
 
 request('http://www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html', function (error, response, body) {
-    fs.writeFile("./result.txt", body);
+    fs.writeFile("./result.txt", body, (fileError)=>
+    {
+        console.log(fileError)
+    });
     console.log(`written to file: result.txt`)
 });
