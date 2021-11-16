0bd3c66ed892cedd106ec662759c1215001974cf
Michael Melena <melena@havit.cz>
2021-11-16 18:34:33 +0100

    updated get_data.js

--- data/get_data.js
+++ data/get_data.js
@@ -1,10 +1,18 @@
-const request = require('request');
-const fs = require('fs');
+const request = require("request");
+const fs = require("fs");
 
-request('http://www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html', function (error, response, body) {
-    fs.writeFile("./result.txt", body, (fileError)=>
-    {
-        console.log(fileError)
-    });
-    console.log(`written to file: result.txt`)
-});
+request(
+  "http://www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html",
+  function (error, response, body) {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log(body);
+      fs.writeFile("data/result.txt", body, (fileError) => {
+        console.log(fileError);
+      });
+    }
+
+    console.log(`written to file: result.txt`);
+  }
+);
